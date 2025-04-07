fcbc4aca2ca678170dbc6ea9b665d8683736736f
Titova Solomia <titovasolomia@icloud.com>
2025-04-07 14:42:32 +0200

    Lab-4

--- EShopService2/Controllers/ProductController.cs
+++ EShopService2/Controllers/ProductController.cs
@@ -1,43 +1,66 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EShop.Domain.Models;
+using EShop.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+using EShop.Application.Services;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace EShopService.Controllers
+namespace EShopService.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+
+public class ProductController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProductController : ControllerBase
+
+    private readonly ProductService _service;
+
+    public ProductController(ProductService service)
     {
-        // GET: api/<ProductController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        _service = service;
+    }
 
-        // GET api/<ProductController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+    [HttpGet]
+    public IActionResult GetAll() => Ok(_service.GetAllProducts());
 
-        // POST api/<ProductController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
+    // GET api/<ProductController>
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        var product = _service.GetProductById(id);
+        if (product == null) return NotFound();
+        return Ok(product);
+    }
+    // POST api/<ProductController>
+    [HttpPost]
+    public IActionResult Post([FromBody] Product product)
+    {
+        _service.AddProduct(product);
+        return CreatedAtAction(nameof(Get), new { id = product.Id }, product);
+    }
 
-        // PUT api/<ProductController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+    // PUT api/<ProductController>
+    [HttpPut("{id}")]
+    public IActionResult Put(int id, [FromBody] Product product)
+    {
+        if (id != product.Id) return BadRequest();
+        _service.UpdateProduct(product);
+        return NoContent();
+    }
 
-        // DELETE api/<ProductController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+    // DELETE api/<ProductController>
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        _service.DeleteProduct(id);
+        return NoContent();
+    }
+
+    // EXISTS api/<ProductController>
+    [HttpGet("exists/{id}")]
+    public ActionResult<bool> Exists(int id)
+    {
+        var exists = _service.GetProductById(id) != null;
+        return Ok(exists);
     }
 }
